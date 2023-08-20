bdeadc1b1be60015386089f4f977f06fc00c34fb
Miles-Collins <miles.collins@ymail.com>
2023-08-20 14:24:21 -0600

    create store modal on load

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,33 @@
-import './globals.css'
+import "./globals.css";
 
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-import { ClerkProvider } from '@clerk/nextjs'
-import ModalProvider from '@/providers/modal-provider'
+import { ClerkProvider } from "@clerk/nextjs";
+import ModalProvider from "@/providers/modal-provider";
+import prismadb from "@/lib/prismadb";
+import ToasterProvider from "@/providers/toast-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Admin Dashboard',
-  description: 'Admin Dashboard',
-}
+  title: "Admin Dashboard",
+  description: "Admin Dashboard",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToasterProvider />
           <ModalProvider />
           {children}
-          </body>
+        </body>
       </html>
     </ClerkProvider>
   );
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,19 +1,99 @@
-'use client'
+"use client";
 
-import React from 'react'
-import Modal from '../ui/modal'
-import { useStoreModal } from '@/hooks/use-store-modal'
+import * as z from "zod";
+
+import React, { useState } from "react";
+import axios from "axios";
+import Modal from "../ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import toast from "react-hot-toast";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 export default function StoreModal() {
-  const storeModal = useStoreModal()
-  
+  const storeModal = useStoreModal();
+
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+
+      const response = await axios.post("/api/stores", values);
+
+      toast.success("Store created!");
+    } catch (error) {
+      toast.error("Something went wrong.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <Modal 
-    title='Create store' 
-    description='Add a new store to manage products and categories' 
-    isOpen={storeModal.isOpen}
-    onClose={storeModal.onClose}>
-      Future Create Store Form
+    <Modal
+      title="Create store"
+      description="Add a new store to manage products and categories"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        placeholder="E-Commerce"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end">
+                <Button
+                  disabled={loading}
+                  variant="outline"
+                  onClick={storeModal.onClose}
+                >
+                  Cancel
+                </Button>
+                <Button disabled={loading} type="submit">
+                  Continue
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   );
-};
+}
