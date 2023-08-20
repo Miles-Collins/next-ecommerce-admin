26ff1f8e5e1da776d2cee3e8ac1921b0c570733d
Miles-Collins <miles.collins@ymail.com>
2023-08-20 11:04:29 -0600

    store modal

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import Modal from "@/components/ui/modal";
+import { UserButton } from "@clerk/nextjs";
+
+export default function SetupPage() {
+  return (
+  <div className="p-4">
+    Root Page
+  </div>
+  );
+}
---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import React from 'react'
+import Modal from '../ui/modal'
+import { useStoreModal } from '@/hooks/use-store-modal'
+
+export default function StoreModal() {
+  const storeModal = useStoreModal()
+  
+  return (
+    <Modal 
+    title='Create store' 
+    description='Add a new store to manage products and categories' 
+    isOpen={storeModal.isOpen}
+    onClose={storeModal.onClose}>
+      Future Create Store Form
+    </Modal>
+  );
+};
